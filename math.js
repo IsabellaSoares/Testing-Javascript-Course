91b8c3b1c5d5c56c500c2aa75714aef3dccee2fc
IsabellaSoares <isabella.soares.28@gmail.com>
2022-01-03 14:03:44 -0300

    Fundamentals of Testing in JavaScript

---
+++ math.js
@@ -0,0 +1,9 @@
+const sum = (a, b) => a - b;
+
+const sumAsync = async (a, b) => a - b;
+
+const subtract = (a, b) => a - b;
+
+const subtractAsync = async (a, b) => a - b;
+
+module.exports = { sum, subtract, sumAsync, subtractAsync };
